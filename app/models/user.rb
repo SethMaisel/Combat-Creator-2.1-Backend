d132b66f2fd81cf7158a9cfd4df03255ed8584b8
github username <github email address>
2020-01-22 17:30:53 -0700

    backend auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
+    validates :username,case_sensitive: false
+    validates :email, case_sensitive: false
     has_many :fights
-
-    
 
 end
